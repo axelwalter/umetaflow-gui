a70b664e991ec92a1f57db1269ad3a804fce04f2
axelwalter <axel.walter@uni-tuebingen.de>
2022-05-20 17:19:45 +0200

    progress on targeted

--- apps/targeted.py
+++ apps/targeted.py
@@ -2,6 +2,7 @@ import streamlit as st
 import plotly.express as px
 from pyopenms import *
 from pymetabo.helpers import Helper
+from pymetabo.core import FeatureFinderMetaboIdent
 import os
 import pandas as pd
 from utils.filehandler import get_files
@@ -12,16 +13,11 @@ def app():
     if "mzML_files_targeted" not in st.session_state:
         st.session_state.mzML_files_targeted = set(["example_data/mzML/standards_1.mzML",
                                         "example_data/mzML/standards_2.mzML"])
-    if "results_dir_targeted" not in st.session_state:
-        st.session_state.results_dir_targeted = "results"
-    
     if "library_options" not in st.session_state:
         st.session_state.library_options = [os.path.join("example_data", "FeatureFinderMetaboIdent", file) 
                                             for file in os.listdir(os.path.join("example_data", "FeatureFinderMetaboIdent"))]
-    if "library" not in st.session_state:
-        st.session_state.library = pd.read_csv([os.path.join("example_data", "FeatureFinderMetaboIdent", file) 
-                                            for file in os.listdir(os.path.join("example_data", "FeatureFinderMetaboIdent"))][0], sep="\t")
 
+    results_dir = "results_targeted"
     with st.sidebar:
         with st.expander("info", expanded=False):
             st.markdown("""
@@ -33,96 +29,68 @@ Workflow for targeted metabolmics with FeatureFinderMetaboIdent.
         with col1:
             mzML_files = st.multiselect("mzML files", st.session_state.mzML_files_targeted, st.session_state.mzML_files_targeted,
                                         format_func=lambda x: os.path.basename(x)[:-5])
-            st.dataframe(st.session_state.library)
-            select_library = st.selectbox("select library", st.session_state.library_options)
-
         with col2:
             st.markdown("##")
             mzML_button = st.button("Add", help="Add new mzML files.")
+        
+        col1, col2 = st.columns([9,1])
+        with col1:
+            library = st.selectbox("select library", st.session_state.library_options)
+        with col2:
             st.markdown("##")
             load_library = st.button("Add", help="Load a library file.")
+
+        st.dataframe(pd.read_csv(library, sep="\t"))
 
         if mzML_button:
             files = get_files("Open mzML files", [("MS Data", ".mzML")])
             for file in files:
                 st.session_state.mzML_files_targeted.add(file)
-
-        if select_library:
-            st.session_state.library = pd.read_csv(select_library, sep='\t')
+            st.experimental_rerun()
 
         if load_library:
-            pass
+            new_lib_files = get_files("Open library file(s)", [("Standards library", ".tsv")])
+            for file in new_lib_files:
+                st.session_state.library_options.insert(0, file)
 
-        col1, col2, col3 = st.columns(3)
+        col1, col2, col3, col4 = st.columns([2,2,2,2])
         with col1:
-            unit = col2.radio("mass tolerance unit", ["ppm", "Da"])
-
-        with col3: 
-            time_unit = col2.radio("time unit", ["seconds", "minutes"])
-
-        if unit == "ppm":
-            tolerance = col2.number_input("mass tolerance", 1, 100, 10)
-        elif unit == "Da":
-            tolerance = col2.number_input("mass tolerance", 0.01, 10.0, 0.02)
-
-        run_button = col2.button("Extract Chromatograms!")
+            ffmid_mz = float(st.number_input("extract:mz_window", 0, 1000, 10))
+        with col2:
+            ffmid_peak_width = float(st.number_input("detect:peak_width", 1, 1000, 60))
+        with col3:
+            ffmid_n_isotopes = st.number_input("extract:n_isotopes", 2, 10, 2)
+        with col4: 
+            time_unit = st.radio("time unit", ["seconds", "minutes"])
+            run_button = st.button("Extract Chromatograms!")
 
     if run_button:
-        Helper().reset_directory(st.session_state.results_dir_targeted)
-        masses = []
-        names = []
-        for line in [line for line in masses_input.split('\n') if line != '']:
-            if '=' in line:
-                mass, name = line.split('=')
-            else:
-                mass = line
-                name = ''
-            masses.append(float(mass.strip()))
-            names.append(name.strip())
+        Helper().reset_directory(results_dir)
+        featureXML_dir = Helper().reset_directory(os.path.join(results_dir, "feature_maps"))
         for file in mzML_files:
             with st.spinner("Extracting from: " + file):
                 exp = MSExperiment()
                 MzMLFile().load(file, exp)
                 df = pd.DataFrame()
-                # get BPC always
-                time = []
-                intensity = []
-                for spec in exp:
-                    _, intensities = spec.get_peaks()
-                    rt = spec.getRT()
-                    if time_unit == "minutes":
-                        rt = rt/60
-                    time.append(rt)
-                    i = int(max(intensities))
-                    intensity.append(i)
-                df["time"] = time
-                df["BPC"] = intensity
-                # get EICs
-                for mass, name in zip(masses, names):
-                    intensity = []
-                    for spec in exp:
-                        _, intensities = spec.get_peaks()
-                        if unit == "Da":
-                            index_highest_peak_within_window = spec.findHighestInWindow(mass, tolerance, tolerance)
-                        else:
-                            index_highest_peak_within_window = spec.findHighestInWindow(mass,float((tolerance/1000000)*mass),float((tolerance/1000000)*mass))
-                        if index_highest_peak_within_window > -1:
-                            intensity.append(int(spec[index_highest_peak_within_window].getIntensity()))
-                        else:
-                            intensity.append(0)
-                    df[str(mass)+"_"+name] = intensity
-            df.to_csv(os.path.join(st.session_state.results_dir_targeted, os.path.basename(file)[:-5]+".tsv"), sep="\t", index=False)
+
+                FeatureFinderMetaboIdent().run(file,
+                            os.path.join(featureXML_dir,  os.path.basename(file[:-4]+"featureXML")), library,
+                            params={"extract:mz_window": ffmid_mz,
+                                    "detect:peak_width": ffmid_peak_width,
+                                    "extract:n_isotopes": ffmid_n_isotopes})
+
+                fm = FeatureMap()
         st.session_state.viewing_targeted = True
 
 
     if st.session_state.viewing_targeted:
-        all_files = st.multiselect("samples", [f for f in os.listdir(st.session_state.results_dir_targeted) if f.endswith(".tsv")], 
-                                [f for f in os.listdir(st.session_state.results_dir_targeted) if f.endswith(".tsv")], format_func=lambda x: os.path.basename(x)[:-4])
+        all_files = st.multiselect("samples", [f for f in os.listdir(results_dir) if f.endswith(".tsv")], 
+                                [f for f in os.listdir(results_dir) if f.endswith(".tsv")], format_func=lambda x: os.path.basename(x)[:-4])
         all_files = sorted(all_files, reverse=True)
         col1, col2 = st.columns([9,1])
-        all_chroms = col1.multiselect("chromatograms", pd.read_csv(os.path.join(st.session_state.results_dir_targeted, os.listdir(st.session_state.results_dir_targeted)[0]),
+        all_chroms = col1.multiselect("chromatograms", pd.read_csv(os.path.join(results_dir, os.listdir(results_dir)[0]),
                                                                 sep="\t").drop(columns=["time"]).columns.tolist(), 
-                                    pd.read_csv(os.path.join(st.session_state.results_dir_targeted, os.listdir(st.session_state.results_dir_targeted)[0]),
+                                    pd.read_csv(os.path.join(results_dir, os.listdir(results_dir)[0]),
                                                 sep="\t").drop(columns=["time"]).columns.tolist())
 
         num_cols = col2.number_input("columns", 1, 5, 1)
@@ -131,7 +99,7 @@ Workflow for targeted metabolmics with FeatureFinderMetaboIdent.
         if col1.button("Download", help="Select a folder where data from all samples gets stored."):
             results_folder = get_result_dir()
             for file in all_files:
-                shutil.copy(os.path.join(st.session_state.results_dir_targeted, file), os.path.join(results_folder, os.path.basename(file)))
+                shutil.copy(os.path.join(results_dir, file), os.path.join(results_folder, os.path.basename(file)))
         cols = st.columns(num_cols)
         while all_files:
             for col in cols:
@@ -139,7 +107,7 @@ Workflow for targeted metabolmics with FeatureFinderMetaboIdent.
                     file = all_files.pop()
                 except IndexError:
                     break
-                df = pd.read_csv(os.path.join(st.session_state.results_dir_targeted, file), sep="\t")
+                df = pd.read_csv(os.path.join(results_dir, file), sep="\t")
 
                 fig = px.line(df, x=df["time"], y=all_chroms)
                 fig.update_layout(xaxis=dict(title="time"), yaxis=dict(title="intensity (cps)"))

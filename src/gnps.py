ac481b690a5f65b1bfdf45aa53df7a7c71687808
axelwalter <axel.walter@uni-tuebingen.de>
2023-01-09 14:54:55 +0100

    enable gnps

--- src/gnps.py
+++ src/gnps.py
@@ -8,6 +8,7 @@ class GNPSExport:
     def run(self, consensusXML_file, aligned_mzML_dir, gnps_dir):
         Helper().reset_directory(gnps_dir)
         mzML_files = [str(f) for f in Path(aligned_mzML_dir).iterdir() if f.is_file() and str(f).endswith("mzML")]
+        
         consensus_map = ConsensusMap()
         ConsensusXMLFile().load(consensusXML_file, consensus_map)
         filtered_map = ConsensusMap(consensus_map)
@@ -20,7 +21,7 @@ class GNPSExport:
         ConsensusXMLFile().store(consensusXML_file, filtered_map)
 
         # for FFBM
-        # GNPSMGFFile().store(String(consensusXML_file), [file.encode() for file in mzML_files], String(os.path.join(gnps_dir, "MS2.mgf")))
+        GNPSMGFFile().store(String(consensusXML_file), [file.encode() for file in mzML_files], String(os.path.join(gnps_dir, "MS2.mgf")))
         GNPSQuantificationFile().store(consensus_map, os.path.join(gnps_dir, "FeatureQantificationTable.txt"))
         GNPSMetaValueFile().store(consensus_map, os.path.join(gnps_dir, "MetaValueTable.tsv"))
 

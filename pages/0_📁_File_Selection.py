b5a62173cd37225d88bcc5dc207c6e12b1ac1247
axelwalter <axel.walter@uni-tuebingen.de>
2023-01-10 15:13:18 +0100

    clarify file selection

--- pages/0_📁_File_Selection.py
+++ pages/0_📁_File_Selection.py
@@ -1,25 +1,24 @@
 import streamlit as st
 from src.helpers import Helper
 from pathlib import Path
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
 st.title("File Selection")
 
-st.markdown("""
+st.info("""
 **How to upload files**
 
 1. Browse files on your computer
-2. Click the Upload mzML files button to use them with this app
+2. Click the **Add mzML files** button to use them in the workflows
 
 The currently uploaded files will be used for data analysis in both workflows.
-#
 """)
 with st.form("my-form", clear_on_submit=True):
         uploaded_mzML = st.file_uploader("mzML files", accept_multiple_files=True)
         _, c2, _ = st.columns(3)
-        submitted = c2.form_submit_button("Upload mzML files")
-# uploaded_mzML = st.file_uploader("mzML files", accept_multiple_files=True)
+        submitted = c2.form_submit_button("Add the uploaded mzML files")
 
 mzML_dir = "mzML_files"
 
@@ -30,27 +29,17 @@ if submitted:
             if file.name not in Path(mzML_dir).iterdir():
                 with open(Path(mzML_dir, file.name),"wb") as f:
                         f.write(file.getbuffer())
-        st.success("Uploaded all files!")
+        st.success("Successfully added uploaded files!")
     else:
-        st.warning("Add some files before uploading them.")
+        st.warning("Upload some files before adding them.")
 
 
-to_clear =[] 
 st.markdown("##")
-st.markdown("### Currently uploaded files")
-for file in Path(mzML_dir).iterdir():
-    if st.checkbox(str(file.stem)):
-        to_clear.append(file)
-st.markdown("##")
-
-c1, c2 = st.columns(2)
-clear_selection = c1.button("Clear Selection")
-clear_all = c2.button("Clear All")
-
-if clear_selection:
-    for file in to_clear:
-        file.unlink()
-    st.experimental_rerun()
+c1, c2 = st.columns([0.4, 0.8])
+c2.markdown("#")
+clear_all = c2.button("Remove All Files")
+c1.markdown("**mzML files for data anaylsis**")
+c1.dataframe(pd.DataFrame({"files": [str(file.stem) for file in Path(mzML_dir).iterdir()]}))
 
 if clear_all:
     Helper().reset_directory("mzML_files")
--- unused/4_🚧_Testing.py
+++ unused/4_🚧_Testing.py
@@ -3,35 +3,11 @@ import streamlit as st
 st.write("for testing...")
 
 
-# from zipfile import ZipFile
-# import base64
 
-# zipObj = ZipFile("sample.zip", "w")
-# # Add multiple files to the zip
-# zipObj.write("results_metabolomics/")
-# # zipObj.write("results_metabolomics/MetaData.tsv")
-# # close the Zip File
-# zipObj.close()
+uploaded_f = st.sidebar.file_uploader("Choose a CSV file", accept_multiple_files=True)
 
-# ZipfileDotZip = "sample.zip"
-
-# with open(ZipfileDotZip, "rb") as f:
-#     bytes = f.read()
-#     b64 = base64.b64encode(bytes).decode()
-#     href = f"<a href=\"data:file/zip;base64,{b64}\" download='{ZipfileDotZip}.zip'>\
-#         Click last model weights\
-#     </a>"
-
-# st.sidebar.markdown(href, unsafe_allow_html=True)
-
-
-import shutil
-shutil.make_archive("sample", 'zip', "results_metabolomics")
-
-with open("sample.zip", "rb") as fp:
-    btn = st.download_button(
-        label="Download ZIP",
-        data=fp,
-        file_name="myfile.zip",
-        mime="application/zip"
-    )
+if uploaded_f is not None:
+    @st.experimental_memo
+    def kimiwa():
+        uploaded_file = uploaded_f
+        return uploaded_file

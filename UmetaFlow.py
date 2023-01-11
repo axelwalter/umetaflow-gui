0f0cf0e93bed5d2735b87f0275d1300a2bc73041
axelwalter <axel.walter@uni-tuebingen.de>
2023-01-11 11:05:41 +0100

    catch errors

--- UmetaFlow.py
+++ UmetaFlow.py
@@ -3,17 +3,21 @@ import os
 
 st.set_page_config(layout="wide")
 
-st.session_state.missing_values_before = None
-st.session_state.missing_values_after = None
+try:
+    st.session_state.missing_values_before = None
+    st.session_state.missing_values_after = None
 
-if not os.path.isdir("mzML_files"):
-    os.mkdir("mzML_files")
+    if not os.path.isdir("mzML_files"):
+        os.mkdir("mzML_files")
 
-st.markdown("""
-# UmetaFlow
+    st.markdown("""
+    # UmetaFlow
 
-## A universal tool for metabolomics data analysis
+    ## A universal tool for metabolomics data analysis
 
-## About
-All LC-MS related analysis is done with [pyOpenMS](https://pyopenms.readthedocs.io/en/latest/index.html).
-""")
+    ## About
+    All LC-MS related analysis is done with [pyOpenMS](https://pyopenms.readthedocs.io/en/latest/index.html).
+    """)
+
+except:
+    st.warning("Something went wrong.")
--- pages/0_📁_File_Selection.py
+++ pages/0_📁_File_Selection.py
@@ -6,45 +6,49 @@ import os
 
 st.set_page_config(layout="wide")
 
-if not os.path.isdir("mzML_files"):
-    os.mkdir("mzML_files")
 
-st.title("File Selection")
+try:
+    if not os.path.isdir("mzML_files"):
+        os.mkdir("mzML_files")
 
-st.info("""
-**How to upload files**
+    st.title("File Selection")
 
-1. Browse files on your computer
-2. Click the **Add mzML files** button to use them in the workflows
+    st.info("""
+    **How to upload files**
 
-The currently uploaded files will be used for data analysis in both workflows.
-""")
-with st.form("my-form", clear_on_submit=True):
-        uploaded_mzML = st.file_uploader("mzML files", accept_multiple_files=True)
-        _, c2, _ = st.columns(3)
-        submitted = c2.form_submit_button("Add the uploaded mzML files")
+    1. Browse files on your computer
+    2. Click the **Add mzML files** button to use them in the workflows
 
-mzML_dir = "mzML_files"
+    The currently uploaded files will be used for data analysis in both workflows.
+    """)
+    with st.form("my-form", clear_on_submit=True):
+            uploaded_mzML = st.file_uploader("mzML files", accept_multiple_files=True)
+            _, c2, _ = st.columns(3)
+            submitted = c2.form_submit_button("Add the uploaded mzML files")
 
-# upload mzML files
-if submitted:
-    if uploaded_mzML:
-        for file in uploaded_mzML:
-            if file.name not in Path(mzML_dir).iterdir() and file.name.endswith("mzML"):
-                with open(Path(mzML_dir, file.name),"wb") as f:
-                        f.write(file.getbuffer())
-        st.success("Successfully added uploaded files!")
-    else:
-        st.warning("Upload some files before adding them.")
+    mzML_dir = "mzML_files"
+
+    # upload mzML files
+    if submitted:
+        if uploaded_mzML:
+            for file in uploaded_mzML:
+                if file.name not in Path(mzML_dir).iterdir() and file.name.endswith("mzML"):
+                    with open(Path(mzML_dir, file.name),"wb") as f:
+                            f.write(file.getbuffer())
+            st.success("Successfully added uploaded files!")
+        else:
+            st.warning("Upload some files before adding them.")
 
 
-st.markdown("##")
-c1, c2 = st.columns([0.4, 0.8])
-c2.markdown("#")
-clear_all = c2.button("Remove All Files")
-c1.markdown("**mzML files for data anaylsis**")
-c1.dataframe(pd.DataFrame({"files": [file.name for file in Path(mzML_dir).iterdir()]}))
+    st.markdown("##")
+    c1, c2 = st.columns([0.4, 0.8])
+    c2.markdown("#")
+    clear_all = c2.button("Remove All Files")
+    c1.markdown("**mzML files for data anaylsis**")
+    c1.dataframe(pd.DataFrame({"files": [file.name for file in Path(mzML_dir).iterdir()]}))
 
-if clear_all:
-    Helper().reset_directory("mzML_files")
-    st.experimental_rerun()
+    if clear_all:
+        Helper().reset_directory("mzML_files")
+        st.experimental_rerun()
+except:
+    st.warning("Something went wrong.")

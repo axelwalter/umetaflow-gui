9a6616a7d9476d3f1263f0c8eb01ec7fcee473b0
axelwalter <axel.walter@uni-tuebingen.de>
2023-01-12 08:33:52 +0100

    change working dir in windows version

--- UmetaFlow.py
+++ UmetaFlow.py
@@ -9,6 +9,10 @@ st.session_state.location = "online"
 if "local" in sys.argv:
     st.session_state.location = "local"
 
+# if we run the packaged windows version, we start within the Python directory -> need to change working directory to ..\umetaflow-gui-main
+if "windows" in sys.argv:
+    os.chdir("../umetaflow-gui")
+
 try:
     st.session_state.missing_values_before = None
     st.session_state.missing_values_after = None

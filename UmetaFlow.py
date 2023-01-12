8ffc1b40364aa46803e550886979f3d85bb58216
axelwalter <axel.walter@uni-tuebingen.de>
2023-01-12 10:37:00 +0100

    add icon

--- UmetaFlow.py
+++ UmetaFlow.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 import sys
 
-st.set_page_config(layout="wide")
+st.set_page_config(page_title="UmetaFlow", page_icon="resources/icon.png", layout="wide", initial_sidebar_state="auto", menu_items=None)
 
 st.session_state.location = "online"
 

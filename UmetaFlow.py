1e8d052dd0b1aefe0685cba15eb36b125d2eb3bc
axelwalter <axel.walter@uni-tuebingen.de>
2023-01-16 12:01:01 +0100

    add description to main page

--- UmetaFlow.py
+++ UmetaFlow.py
@@ -59,10 +59,34 @@ You can share this unique workspace ID with other people.
     st.markdown("""
     ## A universal tool for metabolomics data analysis
 
-    This app is based on the [UmetaFlow](https://chemrxiv.org/engage/chemrxiv/article-details/634fb68fdfbd2b6abc5c5fcd) workflow for LC-MS data analysis. UmetaFlow is implemented as a [snakemake pipeline](https://github.com/NBChub/snakemake-UmetaFlow) and as a Python version in [Jupyter notebooks](https://github.com/eeko-kon/pyOpenMS_UmetaFlow).
+    This app is based on the [UmetaFlow](https://chemrxiv.org/engage/chemrxiv/article-details/634fb68fdfbd2b6abc5c5fcd) workflow for LC-MS data analysis. UmetaFlow is implemented as a [snakemake pipeline](https://github.com/NBChub/snakemake-UmetaFlow) and as a Python version in [Jupyter notebooks](https://github.com/eeko-kon/pyOpenMS_UmetaFlow) based on [pyOpenMS](https://pyopenms.readthedocs.io/en/latest/index.html).
 
-    ## About
-    All LC-MS related analysis is done with [pyOpenMS](https://pyopenms.readthedocs.io/en/latest/index.html).
+    Here, we take the powerful UmetFlow algorithms in a simple and easy graphical user interface. In contrast to the pipeline for automatic data processing,
+    this app is tweaked a bit to be used with smaller to medium sample sets and some manual data interpretation. For example the automatic annotation of features via SIRIUS is omitted.
+    Instead we export all the files necessary to run in the SIRIUS GUI tool and manually annotate the result tables via a unique identifier. This method of curated annotation can be interesting if you really want to be confident in your annotations.
+    The same applies for GNPS, here you can export all the files required for Feature Based Molecular Networking and Ion Identity Networking.
+
+    Besides the core UmetaFlow algorithms in the **Metabolomics** tab, you will find additional tabs for **Extracted Ion Chromatograms** and **Statistics**. The data produced here is fully compatible with the web app for [statistical analyis of metabolomics](https://github.com/axelwalter/streamlit-metabolomics-statistics) data.
+    ## Quickstart
+
+    ### Workspaces
+    On the left side of this page you can define a workspace where all your data including uploaded `mzML` files will be stored. Entering a workspace will switch to an existing one or create a new one if it does not exist yet. In the web app, you can share your results via the unique workspace ID. Be careful with sensitive data, anyone with access to this ID can view your data.
+
+    ### File Handling
+    Upload `mzML` files via the **File Upload** tab. The data will be stored in your workspace. With the web app you can upload only one file at a time.
+    Locally there is no limit in files. However, it is recommended to upload large number of files by specifying the path to a directory containing the files.
+
+    Your uploaded files will be shown in the sidebar of all tabs dealing with the files, e.g. the **Metabolomics** tab. Checked file names will be used for analysis.
+
+    Result files are available via specified download buttons or, if run locally, within the workspace directory.
+    ### Workflows
+    
+    #### Extracted Ion Chromatograms
+
+    Simple workflow for the extraction of chromatograms by `m/z` (and optionally `RT` range) value. Produces a **Feature Matrix** file with area under the curve intensities as well as a **Meta Data** template and the chromatogram data for each file.
+    Area intensities of different variants (e.g. adducts or neutral losses) of a metabolite can be combined. Put a `#` with the name first and variant second (e.g. `glucose` and `glucose#[M+Na]+)`  
+    #### Metabolomics (UmetaFlow)
+    #### Statistics
     """)
 except:
     st.warning("Something went wrong.")
--- pages/3_📈_Statistics.py
+++ pages/3_📈_Statistics.py
@@ -31,103 +31,105 @@ def get_heatmap(ord_ft):
 
 st.set_page_config(page_title="UmetaFlow", page_icon="resources/icon.png", layout="wide", initial_sidebar_state="auto", menu_items=None)
 
-# try:
-st.title("Statistics")
-st.info("""### 💡 Important
+try:
+    st.title("Statistics")
+    st.info("""#### 💡 Important
 
 
-We have developed a web app specifically for metabolomics data analyisis. Visit [the GitHub repository](https://github.com/axelwalter/streamlit-metabolomics-statistics) or [**open the app**](https://axelwalter-streamlit-metabol-statistics-for-metabolomics-3ornhb.streamlit.app/) directly from here.
+    We have developed a web app specifically for metabolomics data analyisis. 
 
-### What you need from this app
+    Visit [the GitHub repository](https://github.com/axelwalter/streamlit-metabolomics-statistics) or [**open the app**](https://axelwalter-streamlit-metabol-statistics-for-metabolomics-3ornhb.streamlit.app/) directly from here.
 
-Both Workflows let you download a **Feature Matrix** and a **Meta Data** table in `tsv` format. Edit the meta data by defining sample types (e.g. Sample, Blank or Pool)
-and add at least one more custom attribute column to where your samples differentiate (e.g. ATTRIBUTE_Treatment: antibiotic and control).
-""")
+    #### What you need from this app
 
-st.markdown("##")
-st.markdown("### Basic Statistics")
+    Both Workflows let you download a **Feature Matrix** and a **Meta Data** table in `tsv` format. Edit the meta data by defining sample types (e.g. Sample, Blank or Pool)
+    and add at least one more custom attribute column to where your samples differentiate (e.g. ATTRIBUTE_Treatment: antibiotic and control).
+    """)
 
-st.radio("choose data from workflow", ["Extracted Ion Chromatograms", "Metabolomics"], key="stats-choice")
+    st.markdown("##")
+    st.markdown("### Basic Statistics")
 
-df = pd.DataFrame()
+    st.radio("choose data from workflow", ["Extracted Ion Chromatograms", "Metabolomics"], key="stats-choice")
 
-if st.session_state["stats-choice"] == "Metabolomics":
-    if Path(st.session_state["workspace"], "results-metabolomics", "FeatureMatrix.tsv").is_file():
-        df = pd.read_csv(Path(st.session_state["workspace"], "results-metabolomics", "FeatureMatrix.tsv"), sep="\t")
+    df = pd.DataFrame()
+
+    if st.session_state["stats-choice"] == "Metabolomics":
+        if Path(st.session_state["workspace"], "results-metabolomics", "FeatureMatrix.tsv").is_file():
+            df = pd.read_csv(Path(st.session_state["workspace"], "results-metabolomics", "FeatureMatrix.tsv"), sep="\t")
+        else:
+            st.warning("No metabolomics results found.")
     else:
-        st.warning("No metabolomics results found.")
-else:
-    if Path(st.session_state["workspace"], "results-extract-chromatograms", "summary.tsv").is_file():
-        df = pd.read_csv(Path(st.session_state["workspace"], "results-extract-chromatograms", "summary.tsv"), sep="\t")
-    else:
-        st.warning("No chromatogram extraction results found.")
+        if Path(st.session_state["workspace"], "results-extract-chromatograms", "summary.tsv").is_file():
+            df = pd.read_csv(Path(st.session_state["workspace"], "results-extract-chromatograms", "summary.tsv"), sep="\t")
+        else:
+            st.warning("No chromatogram extraction results found.")
 
-if not df.empty:
-    st.dataframe(df)
-    samples = set([col[:-5].split("#")[0] for col in df.columns if col.endswith(".mzML")])
+    if not df.empty:
+        st.dataframe(df)
+        samples = set([col[:-5].split("#")[0] for col in df.columns if col.endswith(".mzML")])
 
-    st.markdown("##### Choose samples for comparison")
-    st.info("💡 Samples with equal names before a `#` symbol (replicates) will be grouped together.")
-    c1, c2 = st.columns(2)
-    a = c1.selectbox("samples A", samples)
-    b = c2.selectbox("samples B", samples)
+        st.markdown("##### Choose samples for comparison")
+        st.info("💡 Samples with equal names before a `#` symbol (replicates) will be grouped together.")
+        c1, c2 = st.columns(2)
+        a = c1.selectbox("samples A", samples)
+        b = c2.selectbox("samples B", samples)
 
-    if a != b:
-        mean = pd.concat([df[[col for col in df.columns if a in col]].mean(axis=1), df[[col for col in df.columns if b in col]].mean(axis=1)], axis=1).set_index(df.metabolite).rename(columns={0: a, 1: b})
-        change = pd.DataFrame(np.log2(mean[a] / mean[b])).rename(columns={0: "log2 fold change"})
-        std = pd.concat([df[[col for col in df.columns if a in col]].std(axis=1), df[[col for col in df.columns if b in col]].std(axis=1)], axis=1).set_index(df.metabolite).rename(columns={0: a, 1: b})
-        with st.expander("intermediate results"):
-            st.markdown("**Mean intensities**")
-            st.dataframe(mean)
-            st.markdown("**Standard deviations**")
-            st.dataframe(std)
-            st.markdown("**Fold Changes**")
-            st.dataframe(change)
-        
-        # show plots
-        samples = mean.columns.tolist()
-        features = mean.index.tolist()
-        fig = go.Figure()
-        for feature in features:
-            if not std.empty:
-                fig.add_trace(go.Bar(x=samples, y=mean.loc[feature], name=feature, error_y=dict(type="data", array=std.loc[feature], visible=True)))
-            else:
-                fig.add_trace(go.Bar(x=samples, y=mean.loc[feature], name=feature))
-        fig.update_layout(title="Metabolite Intensities", yaxis=dict(title="mean intensity"))
-        st.plotly_chart(fig)
+        if a != b:
+            mean = pd.concat([df[[col for col in df.columns if a in col]].mean(axis=1), df[[col for col in df.columns if b in col]].mean(axis=1)], axis=1).set_index(df.metabolite).rename(columns={0: a, 1: b})
+            change = pd.DataFrame(np.log2(mean[a] / mean[b])).rename(columns={0: "log2 fold change"})
+            std = pd.concat([df[[col for col in df.columns if a in col]].std(axis=1), df[[col for col in df.columns if b in col]].std(axis=1)], axis=1).set_index(df.metabolite).rename(columns={0: a, 1: b})
+            with st.expander("intermediate results"):
+                st.markdown("**Mean intensities**")
+                st.dataframe(mean)
+                st.markdown("**Standard deviations**")
+                st.dataframe(std)
+                st.markdown("**Fold Changes**")
+                st.dataframe(change)
+            
+            # show plots
+            samples = mean.columns.tolist()
+            features = mean.index.tolist()
+            fig = go.Figure()
+            for feature in features:
+                if not std.empty:
+                    fig.add_trace(go.Bar(x=samples, y=mean.loc[feature], name=feature, error_y=dict(type="data", array=std.loc[feature], visible=True)))
+                else:
+                    fig.add_trace(go.Bar(x=samples, y=mean.loc[feature], name=feature))
+            fig.update_layout(title="Metabolite Intensities", yaxis=dict(title="mean intensity"))
+            st.plotly_chart(fig)
 
-        fig = px.bar(std, barmode="group")
-        fig.update_layout(title="log 2 fold change", yaxis=dict(title="log 2 fold change"))
-        st.plotly_chart(fig)
+            fig = px.bar(std, barmode="group")
+            fig.update_layout(title="log 2 fold change", yaxis=dict(title="log 2 fold change"))
+            st.plotly_chart(fig)
 
-    else:
-        st.warning("Choose different samples for comparison.")
+        else:
+            st.warning("Choose different samples for comparison.")
 
-    st.markdown("### Clustering and Heatmap")
-    if st.button("**🔥 Generate Heatmap**"):
-        df_heat = df[[col for col in df.columns if col.endswith(".mzML")]].set_index(df["metabolite"])
-        # df_heat = StandardScaler().fit_transform(df_heat)
-        fig = ff.create_dendrogram(df_heat.T, labels=list(df_heat.T.index))
-        # fig.update_layout(template='plotly_white')
-        fig.update_xaxes(side="top")
-        st.markdown("##### Clustering")
-        st.plotly_chart(fig)
+        # st.markdown("### Clustering and Heatmap")
+        # if st.button("**🔥 Generate Heatmap**"):
+        #     df_heat = df[[col for col in df.columns if col.endswith(".mzML")]].set_index(df["metabolite"])
+        #     # df_heat = StandardScaler().fit_transform(df_heat)
+        #     fig = ff.create_dendrogram(df_heat.T, labels=list(df_heat.T.index))
+        #     # fig.update_layout(template='plotly_white')
+        #     fig.update_xaxes(side="top")
+        #     st.markdown("##### Clustering")
+        #     st.plotly_chart(fig)
 
-        #Heatmap
-        fig = px.imshow(df_heat,y=list(df_heat.index), x=list(df_heat.columns), text_auto=True, aspect="auto",
-                    color_continuous_scale='RdBu_r', range_color=[-3,3])
+        #     #Heatmap
+        #     fig = px.imshow(df_heat,y=list(df_heat.index), x=list(df_heat.columns), text_auto=True, aspect="auto",
+        #                 color_continuous_scale='RdBu_r', range_color=[-3,3])
 
-        fig.update_layout(
-            autosize=False,
-            width=700,
-            height=1200,
-            xaxis_title="",
-            yaxis_title="")
+        #     fig.update_layout(
+        #         autosize=False,
+        #         width=700,
+        #         height=1200,
+        #         xaxis_title="",
+        #         yaxis_title="")
 
-        # fig.update_yaxes(visible=False)
-        fig.update_xaxes(tickangle = 35)
-        st.markdown("##### Heatmap")
-        st.plotly_chart(fig)
+        #     # fig.update_yaxes(visible=False)
+        #     fig.update_xaxes(tickangle = 35)
+        #     st.markdown("##### Heatmap")
+        #     st.plotly_chart(fig)
 
-# except:
-#     st.warning("Something went wrong.")
+except:
+    st.warning("Something went wrong.")

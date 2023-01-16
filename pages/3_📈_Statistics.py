239ca3cad96f10ac41b758b4deaea052f61e3119
axelwalter <axel.walter@uni-tuebingen.de>
2023-01-16 10:47:52 +0100

    clustering and heatmap

--- pages/3_📈_Statistics.py
+++ pages/3_📈_Statistics.py
@@ -2,8 +2,32 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 import numpy as np
+import plotly.figure_factory as ff
 import plotly.express as px
 import plotly.graph_objects as go
+# from sklearn.preprocessing import StandardScaler
+
+def get_dendrogram(scaled, label_pos="bottom"):
+    fig = ff.create_dendrogram(scaled, labels=list(scaled.index))
+    fig.update_layout(template='plotly_white')
+    fig.update_xaxes(side=label_pos)
+    return fig
+
+def get_heatmap(ord_ft):
+    #Heatmap
+    fig = px.imshow(ord_ft,y=list(ord_ft.index), x=list(ord_ft.columns), text_auto=True, aspect="auto",
+                color_continuous_scale='PuOr_r', range_color=[-3,3])
+
+    fig.update_layout(
+        autosize=False,
+        width=700,
+        height=1200,
+        xaxis_title="",
+        yaxis_title="")
+
+    # fig.update_yaxes(visible=False)
+    fig.update_xaxes(tickangle = 35)
+    return fig
 
 st.set_page_config(page_title="UmetaFlow", page_icon="resources/icon.png", layout="wide", initial_sidebar_state="auto", menu_items=None)
 
@@ -12,16 +36,16 @@ try:
     st.info("""### 💡 Important
 
 
-    We have developed a web app specifically for metabolomics data analyisis. Visit [the GitHub repository](https://github.com/axelwalter/streamlit-metabolomics-statistics) or [**open the app**](https://axelwalter-streamlit-metabol-statistics-for-metabolomics-3ornhb.streamlit.app/) directly from here.
+We have developed a web app specifically for metabolomics data analyisis. Visit [the GitHub repository](https://github.com/axelwalter/streamlit-metabolomics-statistics) or [**open the app**](https://axelwalter-streamlit-metabol-statistics-for-metabolomics-3ornhb.streamlit.app/) directly from here.
 
-    ### What you need from this app
+### What you need from this app
 
-    Both Workflows let you download a **Feature Matrix** and a **Meta Data** table in `tsv` format. Edit the meta data by defining sample types (e.g. Sample, Blank or Pool)
-    and add at least one more custom attribute column to where your samples differentiate (e.g. ATTRIBUTE_Treatment: antibiotic and control).
+Both Workflows let you download a **Feature Matrix** and a **Meta Data** table in `tsv` format. Edit the meta data by defining sample types (e.g. Sample, Blank or Pool)
+and add at least one more custom attribute column to where your samples differentiate (e.g. ATTRIBUTE_Treatment: antibiotic and control).
     """)
 
     st.markdown("##")
-    st.markdown("### Calculate Fold Changes")
+    st.markdown("### Basic Statistics")
 
     st.radio("choose data from workflow", ["Extracted Ion Chromatograms", "Metabolomics"], key="stats-choice")
 
@@ -42,7 +66,7 @@ try:
         st.dataframe(df)
         samples = set([col[:-5].split("#")[0] for col in df.columns if col.endswith(".mzML")])
 
-        st.markdown("**Choose samples for comparison**")
+        st.markdown("##### Choose samples for comparison")
         st.info("💡 Samples with equal names before a `#` symbol (replicates) will be grouped together.")
         c1, c2 = st.columns(2)
         a = c1.selectbox("samples A", samples)
@@ -57,7 +81,7 @@ try:
                 st.dataframe(mean)
                 st.markdown("**Standard deviations**")
                 st.dataframe(std)
-                st.markdown("**Standard deviations**")
+                st.markdown("**Fold Changes**")
                 st.dataframe(change)
             
             # show plots
@@ -78,6 +102,34 @@ try:
 
         else:
             st.warning("Choose different samples for comparison.")
+
+        c1, c2 = st.columns(2)
+        c1.markdown("### Clustering and Heatmap")
+        c2.markdown(" ")
+        if c2.button("**🔥 Generate Heatmap**"):
+            df_heat = df[[col for col in df.columns if col.endswith(".mzML")]].set_index(df["metabolite"]).T
+            # df_heat = StandardScaler().fit_transform(df_heat)
+            fig = ff.create_dendrogram(df_heat, labels=list(df_heat.index))
+            fig.update_layout(template='plotly_white')
+            fig.update_xaxes(side="top")
+            st.markdown("##### Clustering")
+            st.plotly_chart(fig)
+
+            #Heatmap
+            fig = px.imshow(df_heat,y=list(df_heat.index), x=list(df_heat.columns), text_auto=True, aspect="auto",
+                        color_continuous_scale='PuOr_r', range_color=[-3,3])
+
+            fig.update_layout(
+                autosize=False,
+                width=700,
+                height=1200,
+                xaxis_title="",
+                yaxis_title="")
+
+            # fig.update_yaxes(visible=False)
+            fig.update_xaxes(tickangle = 35)
+            st.markdown("##### Heatmap")
+            st.plotly_chart(fig)
 
 except:
     st.warning("Something went wrong.")

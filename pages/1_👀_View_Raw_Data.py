7c3efd4e62583f72bcbd153e4e80743b073a334d
axelwalter <axel.walter@uni-tuebingen.de>
2023-03-22 12:02:04 +0100

    click to show spectra

--- pages/1_👀_View_Raw_Data.py
+++ pages/1_👀_View_Raw_Data.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 from src.view import *
 from src.constants import ERRORS
 from streamlit_plotly_events import plotly_events
@@ -20,73 +21,78 @@ def content():
     st.session_state.view_spectra_dict = get_spectra_dict(st.session_state["mzML_dfs"])
 
     c1, c2 = st.columns(2)
-    st.session_state.view_file = c1.selectbox(
-        "choose file",
-        st.session_state.view_spectra_dict.keys(),
+    c1.selectbox(
+        "choose file", st.session_state.view_spectra_dict.keys(), key="view_file"
     )
 
-    st.session_state.view_df_ms1, st.session_state.view_df_ms2 = get_dfs(
+    st.session_state.view_df_MS1, st.session_state.view_df_MS2 = get_dfs(
         st.session_state.view_spectra_dict, st.session_state.view_file
     )
 
-    c1.select_slider(
-        "MS1 spectrum (RT) 💡 Scroll with the arrow keys!",
-        st.session_state.view_df_ms1["RT"].round(2),
-        key="ms1_RT",
-    )
-    c2.number_input(
-        "2D map intensity cutoff",
-        1000,
-        1000000000,
-        5000,
-        1000,
-        key="cutoff",
-    )
-    c2.selectbox(
-        "MS2 spectrum",
-        sorted(
-            [
-                f"{mz} m/z @ {rt} s"
-                for mz, rt in zip(
-                    st.session_state.view_df_ms2["precursormz"].round(4),
-                    st.session_state.view_df_ms2["RT"].round(2),
-                )
+    if not st.session_state.view_df_MS1.empty:
+        st.markdown("### Peak Map and MS2 spectra")
+        c1, c2 = st.columns(2)
+        c1.number_input(
+            "2D map intensity cutoff",
+            1000,
+            1000000000,
+            5000,
+            1000,
+            key="cutoff",
+        )
+        if not st.session_state.view_df_MS2.empty:
+            c2.markdown("##")
+            c2.markdown("💡 Click anywhere to show the closest MS2 spectrum.")
+        st.session_state.view_fig_map = plot_2D_map(
+            st.session_state.view_df_MS1,
+            st.session_state.view_df_MS2,
+            st.session_state.cutoff,
+        )
+        # Determine RT and mz positions from clicks in the map to get closest MS2 spectrum
+        if not st.session_state.view_df_MS2.empty:
+            map_points = plotly_events(st.session_state.view_fig_map)
+            if map_points:
+                rt = map_points[0]["x"]
+                prec_mz = map_points[0]["y"]
+            else:
+                rt = st.session_state.view_df_MS2.iloc[0, 2]
+                prec_mz = st.session_state.view_df_MS2.iloc[0, 0]
+            spec = st.session_state.view_df_MS2.loc[
+                (
+                    abs(st.session_state.view_df_MS2["RT"] - rt)
+                    + abs(st.session_state.view_df_MS2["precursormz"] - prec_mz)
+                ).idxmin(),
+                :,
             ]
-        ),
-        key="ms2_spec",
-    )
-
-    if not st.session_state.view_df_ms2.empty:
-        ms2_precmz = float(st.session_state.ms2_spec.split(" m/z")[0])
-        ms2_RT = st.session_state.view_df_ms2[
-            st.session_state.view_df_ms2["precursormz"].round(4) == ms2_precmz
-        ]["RT"].tolist()[0]
-    else:
-        ms2_precmz = 0
-        ms2_RT = 0
-
-    if not st.session_state.view_df_ms1.empty:
-        plot_2D_map(st.session_state.view_df_ms1, st.session_state.cutoff)
-        st.session_state.view_fig_bpc = plot_bpc(
-            st.session_state.view_df_ms1, st.session_state.ms1_RT, ms2_RT
-        )
-        st.plotly_chart(st.session_state.view_fig_bpc, use_container_width=True)
-
-        plot_ms_spectrum(
-            st.session_state.view_df_ms1[
-                st.session_state.view_df_ms1["RT"].round(2) == st.session_state.ms1_RT
-            ],
-            f"MS1 spectrum @RT {st.session_state.ms1_RT}",
-            "#EF553B",
-        )
-        if not st.session_state.view_df_ms2.empty:
             plot_ms_spectrum(
-                st.session_state.view_df_ms2[
-                    st.session_state.view_df_ms2["precursormz"].round(4) == ms2_precmz
-                ],
-                f"MS2 spectrum @precursor m/z {ms2_precmz} @RT {round(ms2_RT, 2)}",
+                spec,
+                f"MS2 spectrum @precursor m/z {round(spec['precursormz'], 4)} @RT {round(spec['RT'], 2)}",
                 "#00CC96",
             )
+        else:
+            st.plotly_chart(st.session_state.view_fig_map, use_container_width=True)
+
+        # BPC and MS1 spec
+        st.markdown("### Base Peak Chromatogram (BPC)")
+        st.markdown("💡 Click a point in the BPC to show the MS1 spectrum.")
+        st.session_state.view_fig_bpc = plot_bpc(st.session_state.view_df_MS1)
+
+        # Determine RT positions from clicks in BPC to show MS1 at this position
+        bpc_points = plotly_events(st.session_state.view_fig_bpc)
+        if bpc_points:
+            st.session_state.view_MS1_RT = bpc_points[0]["x"]
+        else:
+            st.session_state.view_MS1_RT = st.session_state.view_df_MS1.loc[0, "RT"]
+
+        spec = st.session_state.view_df_MS1.loc[
+            st.session_state.view_df_MS1["RT"] == st.session_state.view_MS1_RT
+        ].squeeze()
+
+        plot_ms_spectrum(
+            spec,
+            f"MS1 spectrum @RT {spec['RT']}",
+            "#EF553B",
+        )
 
 
 if __name__ == "__main__":
--- src/view.py
+++ src/view.py
@@ -23,16 +23,18 @@ def get_dfs(spectra, file):
 
 
 @st.cache_resource
-def plot_2D_map(df, cutoff):
+def plot_2D_map(df_ms1, df_ms2, cutoff):
     fig = go.Figure()
-    ints = np.concatenate([df.loc[index, "intarray"] for index in df.index])
-    int_filter = ints > cutoff  # show only ints over threshold
+    ints = np.concatenate([df_ms1.loc[index, "intarray"] for index in df_ms1.index])
+    int_filter = ints > cutoff  # show only ints over cutoff threshold
     ints = ints[int_filter]
-    mzs = np.concatenate([df.loc[index, "mzarray"] for index in df.index])[int_filter]
+    mzs = np.concatenate([df_ms1.loc[index, "mzarray"] for index in df_ms1.index])[
+        int_filter
+    ]
     rts = np.concatenate(
         [
-            np.full(len(df.loc[index, "mzarray"]), df.loc[index, "RT"])
-            for index in df.index
+            np.full(len(df_ms1.loc[index, "mzarray"]), df_ms1.loc[index, "RT"])
+            for index in df_ms1.index
         ]
     )[int_filter]
 
@@ -51,13 +53,26 @@ def plot_2D_map(df, cutoff):
             marker_symbol="square",
         )
     )
+
+    # Add MS2 precursors
+    fig.add_trace(
+        go.Scattergl(
+            name="peaks",
+            x=df_ms2["RT"],
+            y=df_ms2["precursormz"],
+            mode="markers",
+            marker_color="#00FF00",
+            marker_symbol="x",
+        )
+    )
     fig.update_layout(
-        title="peak map 2D",
+        # title="peak map 2D",
         xaxis_title="retention time",
         yaxis_title="m/z",
         plot_bgcolor="rgb(255,255,255)",
-        # height=800,
+        showlegend=False,
         # width=1000,
+        # height=800,
     )
     fig.layout.template = "plotly_white"
 
@@ -75,64 +90,62 @@ def plot_2D_map(df, cutoff):
         hovertext=ints.round(),
         selector=dict(type="scattergl"),
     )
-    st.plotly_chart(fig, use_container_width=True)
-    return
+    return fig
 
 
 @st.cache_resource
-def plot_bpc(df, ms1_rt, ms2_rt=0):
-    import time
-
-    time.sleep(4)
+def plot_bpc(df):
     intensity = np.array([max(intensity_array) for intensity_array in df["intarray"]])
     fig = px.line(df, x="RT", y=intensity)
-    fig.add_trace(
-        go.Scatter(
-            x=[ms1_rt],
-            y=[intensity[np.abs(df["RT"] - ms1_rt).argmin()]],
-            name="MS1 spectrum",
-            text="MS1",
-            textposition="top center",
-            textfont=dict(color="#EF553B", size=20),
-        )
-    )
-    fig.data[1].update(
-        mode="markers+text",
-        marker_symbol="x",
-        marker=dict(color="#EF553B", size=12),
-    )
-    if ms2_rt > 0:
-        fig.add_trace(
-            go.Scatter(
-                x=[ms2_rt],
-                y=[intensity[np.abs(df["RT"] - ms2_rt).argmin()]],
-                name="MS2 spectrum",
-                text="MS2",
-                textposition="top center",
-                textfont=dict(color="#00CC96", size=20),
-            )
-        )
-        fig.data[2].update(
-            mode="markers+text",
-            marker_symbol="x",
-            marker=dict(color="#00CC96", size=12),
-        )
+    fig.update_traces(line_color="#555FF5", line_width=3)
+    # fig.add_trace(
+    #     go.Scatter(
+    #         x=[ms1_rt],
+    #         y=[intensity[np.abs(df["RT"] - ms1_rt).argmin()]],
+    #         name="MS1 spectrum",
+    #         text="MS1",
+    #         textposition="top center",
+    #         textfont=dict(color="#EF553B", size=20),
+    #     )
+    # )
+    # fig.data[1].update(
+    #     mode="markers+text",
+    #     marker_symbol="x",
+    #     marker=dict(color="#EF553B", size=12),
+    # )
+    # if ms2_rt > 0:
+    #     fig.add_trace(
+    #         go.Scatter(
+    #             x=[ms2_rt],
+    #             y=[intensity[np.abs(df["RT"] - ms2_rt).argmin()]],
+    #             name="MS2 spectrum",
+    #             text="MS2",
+    #             textposition="top center",
+    #             textfont=dict(color="#00CC96", size=20),
+    #         )
+    #     )
+    #     fig.data[2].update(
+    #         mode="markers+text",
+    #         marker_symbol="x",
+    #         marker=dict(color="#00CC96", size=12),
+    #     )
     fig.update_traces(showlegend=False)
     fig.update_layout(
         showlegend=False,
-        title_text="base peak chromatogram (BPC)",
+        # title_text="base peak chromatogram (BPC)",
         xaxis_title="retention time (s)",
         yaxis_title="intensity (cps)",
         plot_bgcolor="rgb(255,255,255)",
+        width=1000,
     )
     fig.layout.template = "plotly_white"
     return fig
 
 
 @st.cache_resource
-def plot_ms_spectrum(df_spectrum, title, color):
+def plot_ms_spectrum(spec, title, color):
     """
-    Takes a pandas Dataframe with one row (spectrum) and generates a needle plot with m/z and intensity dimension.
+    Takes a pandas Series (spec) and generates a needle plot with m/z and intensity dimension.
     """
 
     def create_spectra(x, y, zero=0):
@@ -141,9 +154,7 @@ def plot_ms_spectrum(df_spectrum, title, color):
         y[::3] = y[2::3] = zero
         return pd.DataFrame({"mz": x, "intensity": y})
 
-    df = create_spectra(
-        df_spectrum["mzarray"].tolist()[0], df_spectrum["intarray"].tolist()[0]
-    )
+    df = create_spectra(spec["mzarray"], spec["intarray"])
     fig = px.line(df, x="mz", y="intensity")
     fig.update_traces(line_color=color)
     fig.update_layout(

26977d81810b1e997e6fa3f3cf568add7454963f
axelwalter <axel.walter@uni-tuebingen.de>
2023-09-05 14:01:38 +0200

    run UmetaFlow local

--- runUmetaFlow.py
+++ runUmetaFlow.py
@@ -1,5 +1,5 @@
 from streamlit.web import cli
 
 if __name__=='__main__':
-    cli._main_run_clExplicit('UmetaFlow.py', 'streamlit run')
+    cli._main_run_clExplicit(['streamlit', 'run', 'UmetaFlow.py', 'local'])
     # we will create this function inside our streamlit framework

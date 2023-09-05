fe54a864d90703ab57cd66079d8a97f5fd9456c3
axelwalter <axel.walter@uni-tuebingen.de>
2023-09-05 15:30:24 +0200

    run with local arg

--- run_app.py
+++ run_app.py
@@ -1,6 +1,6 @@
 from streamlit.web import cli
 
 if __name__=='__main__':
-    cli._main_run_clExplicit('app.py', 'local', 'streamlit run')
+    cli._main_run_clExplicit(['app.py', 'local'], 'streamlit run')
 
     # we will create this function inside our streamlit framework

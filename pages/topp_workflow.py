f54f40490c1e8c4127a6b7df691df7bbc10d6e41
axelwalter <axel.walter@uni-tuebingen.de>
2024-07-09 15:29:29 +0200

    remove name-main in topp workflow to work with st.navigation

--- pages/topp_workflow.py
+++ pages/topp_workflow.py
@@ -2,25 +2,23 @@ import streamlit as st
 from src.common import page_setup
 from src.Workflow import Workflow
 
-# The rest of the page can, but does not have to be changed
-if __name__ == "__main__":
+# # The rest of the page can, but does not have to be changed
+params = page_setup()
+
+wf = Workflow()
+
+st.title(wf.name)
+
+t = st.tabs(["📁 **File Upload**", "⚙️ **Configure**", "🚀 **Run**", "📊 **Results**"])
+with t[0]:
+    wf.show_file_upload_section()
+
+with t[1]:
+    wf.show_parameter_section()
+
+with t[2]:
+    wf.show_execution_section()
     
-    params = page_setup()
-
-    wf = Workflow()
-
-    st.title(wf.name)
-
-    t = st.tabs(["📁 **File Upload**", "⚙️ **Configure**", "🚀 **Run**", "📊 **Results**"])
-    with t[0]:
-        wf.show_file_upload_section()
-
-    with t[1]:
-        wf.show_parameter_section()
-
-    with t[2]:
-        wf.show_execution_section()
-        
-    with t[3]:
-        wf.show_results_section()
+with t[3]:
+    wf.show_results_section()
 

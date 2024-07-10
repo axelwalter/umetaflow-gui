b8d28284fd193a0f7cbc501a0633376773eb8619
axelwalter <axel.walter@uni-tuebingen.de>
2024-07-10 09:25:09 +0200

    update TOPP workflow example - simple workflow with feature detection and linking - export consensus map to dataframe with python script - added interactive result section

--- src/Workflow.py
+++ src/Workflow.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from .workflow.WorkflowManager import WorkflowManager
 
+# for result section:
+from pathlib import Path
+import pandas as pd
+import plotly.express as px
+from .common import show_fig
+
 class Workflow(WorkflowManager):
     # Setup pages for upload, parameter, execution and results.
     # For layout use any streamlit components such as tabs (as shown in example), columns, or even expanders.
@@ -23,20 +29,17 @@ class Workflow(WorkflowManager):
 
         # Create tabs for different analysis steps.
         t = st.tabs(
-            ["**Feature Detection**", "**Adduct Detection**", "**SIRIUS Export**", "**Python Custom Tool**"]
+            ["**Feature Detection**", "**Feature Linking**", "**Python Custom Tool**"]
         )
         with t[0]:
             # Parameters for FeatureFinderMetabo TOPP tool.
             self.ui.input_TOPP("FeatureFinderMetabo", custom_defaults={"algorithm:common:noise_threshold_int": 1000.0})
         with t[1]:
-            # A single checkbox widget for workflow logic.
-            self.ui.input_widget("run-adduct-detection", False, "Adduct Detection")
             # Paramters for MetaboliteAdductDecharger TOPP tool.
-            self.ui.input_TOPP("MetaboliteAdductDecharger")
+            self.ui.input_TOPP("FeatureLinkerUnlabeledKD")
         with t[2]:
-            # Paramters for SiriusExport TOPP tool
-            self.ui.input_TOPP("SiriusExport")
-        with t[3]:
+            # A single checkbox widget for workflow logic.
+            self.ui.input_widget("run-python-script", False, "Run custom Python script")
             # Generate input widgets for a custom Python tool, located at src/python-tools.
             # Parameters are specified within the file in the DEFAULTS dictionary.
             self.ui.input_python("example")
@@ -57,27 +60,42 @@ class Workflow(WorkflowManager):
         out_ffm = self.file_manager.get_files(in_mzML, "featureXML", "feature-detection")
 
         # Run FeatureFinderMetabo tool with input and output files.
+        self.logger.log("Detecting features...")
         self.executor.run_topp(
             "FeatureFinderMetabo", input_output={"in": in_mzML, "out": out_ffm}
         )
 
+        # Prepare input and output files for feature linking
+        in_fl = self.file_manager.get_files(out_ffm, collect=True)
+        out_fl = self.file_manager.get_files("feature_matrix.consensusXML", set_results_dir="feature-linking")
+
+        # Run FeatureLinkerUnlabaeledKD with all feature maps passed at once
+        self.logger.log("Linking features...")
+        self.executor.run_topp("FeatureLinkerUnlabeledKD", input_output={"in": in_fl, "out": out_fl})
+        self.logger.log("Exporting consensus features to pandas DataFrame...")
+        self.executor.run_python("export_consensus_feature_df", input_output={"in": out_fl[0]})
         # Check if adduct detection should be run.
-        if self.params["run-adduct-detection"]:
-        
-            # Run MetaboliteAdductDecharger for adduct detection, with disabled logs.
-            # Without a new file list for output, the input files will be overwritten in this case.
-            self.executor.run_topp(
-                "MetaboliteAdductDecharger", {"in": out_ffm, "out_fm": out_ffm}
-            )
-
-        # Example for a custom Python tool, which is located in src/python-tools.
-        self.executor.run_python("example", {"in": in_mzML})
-
-        # Prepare output file for SiriusExport.
-        out_se = self.file_manager.get_files("sirius.ms", set_results_dir="sirius-export")
-        self.executor.run_topp("SiriusExport", {"in": self.file_manager.get_files(in_mzML, collect=True),
-                                                "in_featureinfo": self.file_manager.get_files(out_ffm, collect=True),
-                                                "out": out_se})
+        if self.params["run-python-script"]:
+            # Example for a custom Python tool, which is located in src/python-tools.
+            self.executor.run_python("example", {"in": in_mzML})
 
     def results(self) -> None:
-        st.warning("Not implemented yet.")
+        @st.experimental_fragment
+        def show_consensus_features():
+                df = pd.read_csv(file, sep="\t", index_col=0)
+                st.metric("number of consensus features", df.shape[0])
+                c1, c2 = st.columns(2)
+                rows = c1.dataframe(df, selection_mode="multi-row", on_select="rerun")["selection"]["rows"]
+                if rows:
+                    df = df.iloc[rows, 4:]
+                    fig = px.bar(df, barmode="group", labels={"value": "intensity"})
+                    with c2:
+                        show_fig(fig, "consensus-feature-intensities")
+                else:
+                    st.info("💡 Select one ore more rows in the table to show a barplot with intensities.")
+
+        file = Path(self.workflow_dir, "results", "feature-linking", "feature_matrix.tsv")
+        if file.exists():
+            show_consensus_features()
+        else:
+            st.warning("No consensus feature file found. Please run workflow first.")

f3dad89dd6ce4ac4ca9a68598cdc451928960518
singjc <justincsing@gmail.com>
2024-08-20 13:11:53 -0400

    remove: old st.dataframe call

--- src/view.py
+++ src/view.py
@@ -222,21 +222,6 @@ def view_spectrum():
     with cols[0]:
         df = st.session_state.view_spectra.copy()
         df["spectrum ID"] = df.index + 1
-        
-        # event = st.dataframe(
-        #     df,
-        #     column_order=[
-        #         "spectrum ID",
-        #         "RT",
-        #         "MS level",
-        #         "max intensity m/z",
-        #         "precursor m/z",
-        #     ],
-        #     selection_mode="single-row",
-        #     on_select="rerun",
-        #     use_container_width=True,
-        #     hide_index=True,
-        # )
         event = display_large_dataframe(df)
         rows = event.selection.rows
     with cols[1]:

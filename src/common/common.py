6e01005b86d492741511068bdf81cbf627ff57e6
Tom David Müller <57191390+t0mdavid-m@users.noreply.github.com>
2024-10-10 14:51:14 +0200

    add test

--- src/common/common.py
+++ src/common/common.py
@@ -149,7 +149,7 @@ def page_setup(page: str = "") -> dict[str, Any]:
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','GTM-NRXGDF5H');</script>
+                    })(window,document,'script','dataLayer','GTM-NRXGDF5H');console.log('test');</script>
                     <!-- End Google Tag Manager -->
                 </head>
                 <body>

bc678aeac3f7eb2a2538deb14287e0949741208c
Tom David Müller <57191390+t0mdavid-m@users.noreply.github.com>
2024-10-12 17:40:03 +0200

    Update common.py

--- src/common/common.py
+++ src/common/common.py
@@ -144,20 +144,15 @@ def page_setup(page: str = "") -> dict[str, Any]:
             <!DOCTYPE html>
             <html lang="en">
                 <head>
-                    <!-- Google Tag Manager -->
-                    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','GTM-NRXGDF5H');</script>
-                    <!-- End Google Tag Manager -->
+                  window.parent.dataLayer = window.parent.dataLayer || [];
+                  function gtag(){window.parent.dataLayer.push(arguments);}
                 </head>
                 <body>
-                <!-- Google Tag Manager (noscript) -->
-                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NRXGDF5H"
-                height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-                <!-- End Google Tag Manager (noscript) -->
-                Test
+                gtag('event', 'submit_form', {
+                  'event_category': 'form_interaction',
+                  'event_label': 'Submit Form Button',
+                  'value': 1
+                });
                 </body>
             </html>
             """,

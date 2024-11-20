f70c3bddc557ff3cfd51d3724bdd0aece84d3116
Tom David Mueller <tom.mueller@beachouse.de>
2024-11-20 15:58:03 +0100

    fix weird pyopenms bug

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from pathlib import Path
+# For some reason the windows version only works if this is imported here
+import pyopenms
 
 if __name__ == '__main__':
     pages = {
